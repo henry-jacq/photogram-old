1dc34ac5e875d41eee8681fffeba4ed409e31e7c
henry-jacq <henryeditz267@gmail.com>
2023-02-24 17:06:53 +0000

    change: sign in/up styles changed

--- htdocs/_templates/login/form_in.php
+++ htdocs/_templates/login/form_in.php
@@ -3,19 +3,17 @@
 
 <form method="post" action="/login">
     <!-- Brand Logo -->
-    <img class="brand-logo img-fluid mb-3 mx-auto d-block" src="<?=get_config('base_path')?>assets/brand/photogram-brand-big.png"
-        alt="Photogram logo">
+    <img class="brand-logo img-fluid mb-3 mx-auto d-block" src="<?= get_config('base_path') ?>assets/brand/photogram-brand-big.png" alt="Photogram logo">
 
     <h4 class="text-light mb-3 fw-semibold">Sign in</h4>
     <div class="form-floating mb-3">
-        <input type="text" class="form-control rounded-0 form-input border-secondary text-lowercase text-light" id="floatingInput"
-            placeholder="Email or Username" name="username_or_email" required>
+        <input type="text" class="form-control rounded-0 form-input border-secondary text-lowercase text-light" id="floatingInput" placeholder="Email or Username" name="username_or_email" required>
         <label class="text-secondary" for="floatingInput">Email or Username</label>
     </div>
     <div class="form-floating mb-4 input-group">
-            <input type="password" name="password" class="form-control rounded-0 form-input border-secondary text-light" id="floatingPassword" placeholder="Enter your password" required>
-            <span class="input-group-text rounded-0 form-input border-secondary text-light" id="togglePassword"><i id="togglePasswordbtn" class="fa fa-eye-slash"></i></span>
-            <label class="text-secondary" for="floatingPassword">Password</label>
+        <input type="password" name="password" class="form-control rounded-0 form-input border-secondary text-light" id="floatingPassword" placeholder="Enter your password" required>
+        <span class="input-group-text rounded-0 form-input border-secondary text-light" id="togglePassword"><i id="togglePasswordbtn" class="fa fa-eye-slash"></i></span>
+        <label class="text-secondary" for="floatingPassword">Password</label>
     </div>
     <!-- Fingerprint -->
     <input name="visitor_id" type="hidden" class="form-control" id="fingerprint" value="">
@@ -24,7 +22,7 @@
         <label class="text-light">Remember me</label>
     </div>
     <div class="d-grid gap-2 mb-4">
-        <button type="submit" class="btn btn-dracula w-100 rounded-0 hvr-grow">Sign in</button>
+        <button type="submit" class="btn bg-primary hvr-grow w-100 rounded-0">Sign in</button>
     </div>
     <div class="text-white">
         <label for="">Want to join photogram? <a href="/signup" class="text-decoration-none">Register now</a>.</label>
--- htdocs/_templates/signup/form_up.php
+++ htdocs/_templates/signup/form_up.php
@@ -3,32 +3,27 @@
 
 <form method="post" action="/signup">
     <!-- Brand Logo -->
-    <img class="brand-logo img-fluid mb-3 mx-auto d-block" src="<?=get_config('base_path')?>assets/brand/photogram-brand-big.png"
-        alt="Photogram logo">
+    <img class="brand-logo img-fluid mb-3 mx-auto d-block" src="<?= get_config('base_path') ?>assets/brand/photogram-brand-big.png" alt="Photogram logo">
 
     <h4 class="text-light mb-3 fw-semibold">Register</h4>
     <div class="form-floating mb-2">
-        <input type="text" class="form-control rounded-0 form-input border-secondary text-lowercase text-light" id="floatingInput"
-            placeholder="Username" name="username" required>
+        <input type="text" class="form-control rounded-0 form-input border-secondary text-lowercase text-light" id="floatingInput" placeholder="Username" name="username" required>
         <label class="text-secondary" for="floatingInput">Username</label>
     </div>
     <div class="form-floating mb-2">
-        <input type="password" class="form-control rounded-0 form-input border-secondary text-light" id="floatingPassword"
-            placeholder="Password" name="password" required>
+        <input type="password" class="form-control rounded-0 form-input border-secondary text-light" id="floatingPassword" placeholder="Password" name="password" required>
         <label class="text-secondary" for="floatingPassword">Password</label>
     </div>
     <div class="form-floating mb-2">
-        <input type="email" class="form-control rounded-0 form-input border-secondary text-light" id="floatingInput" placeholder="Email"
-            name="email_address" required>
+        <input type="email" class="form-control rounded-0 form-input border-secondary text-light" id="floatingInput" placeholder="Email" name="email_address" required>
         <label class="text-secondary" for="floatingEmail">Email</label>
     </div>
     <div class="form-floating mb-4">
-        <input type="text" class="form-control rounded-0 form-input border-secondary text-light" id="floatingInput"
-            placeholder="Phone Number" name="phone" required>
+        <input type="text" class="form-control rounded-0 form-input border-secondary text-light" id="floatingInput" placeholder="Phone Number" name="phone" required>
         <label class="text-secondary" for="floatingInput">Phone Number</label>
     </div>
     <div class="d-grid gap-2 mb-3">
-        <button type="submit" class="btn btn-dracula w-100 rounded-0 hvr-grow">Create account</button>
+        <button type="submit" class="btn btn-primary w-100 rounded-0 hvr-grow">Create account</button>
     </div>
     <div class="text-white">
         <label>Do you already have an account? <a href="/login" class="text-decoration-none">Login now</a>.</label>
