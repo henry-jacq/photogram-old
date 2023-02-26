6ba2939ffe133beb5356c943ee626dc4ac3572f5
henry-jacq <henryeditz267@gmail.com>
2023-02-26 17:25:30 +0000

    change: footer changed

--- htdocs/_templates/_footer.php
+++ htdocs/_templates/_footer.php
@@ -1,16 +1,31 @@
 <div class="container mt-auto">
-  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-2">
-    <p class="col-md-4 mb-0 text-muted">© 2022 - 2023 Photogram, Inc</p>
-    
+  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <p class="col-md-4 mb-0 text-muted">&copy; 2023 Photogram, Inc</p>
+
     <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-        <img src="/assets/brand/photogram-logo.png" alt="Logo" width="28" height="30" class="d-inline-block align-text-top">
+      <img src="/assets/brand/photogram-logo.png" alt="Logo" width="28" height="30" class="d-inline-block align-text-top">
     </a>
-    
+
     <ul class="nav col-md-4 justify-content-end">
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Go to top  <i class="fa-sharp fa-solid fa-up-long"></i></a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Go to top <i class="bi bi-arrow-up"></i></a></li>
     </ul>
-    <p class="col-lg-12 mt-5 mb-0 text-muted text-center">
-        Made with ❤️ <a href="/" class="text-decoration-none">Photogram</a> by <a href="https://github.com/henry-jacq/" class="text-decoration-none">@Henry</a>.
-    </p>
+    <!-- <div class="justify-content-center align-items-center">
+
+      <p class="text-center text-muted">© 2022 Company, Inc</p>
+    </div> -->
+
   </footer>
+  <!-- <footer class="d-flex flex-wrap justify-content-center align-items-center py-3 my-4 border-top">
+    <footer class="py-3 my-4">
+      <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+      </ul>
+      <p class="text-center text-muted">© 2022 Company, Inc</p>
+    </footer>
+
+  </footer> -->
 </div>
