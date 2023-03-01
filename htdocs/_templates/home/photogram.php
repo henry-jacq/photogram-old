d2a086a36136acd0d4c574e28cfe659b5adeb02a
henry-jacq <henryeditz267@gmail.com>
2023-03-01 17:01:52 +0000

    change: shadow and border removed

--- htdocs/_templates/home/photogram.php
+++ htdocs/_templates/home/photogram.php
@@ -1,3 +1,3 @@
-<div class="album py-5 border-top border-bottom border-secondary shadow" style="background-color: #242829;">
+<div class="album py-5" style="background-color: #242829;">
     <?=Session::loadTemplate('home/show_posts');?>
 </div>
