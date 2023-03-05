cf49c688ce0454f89cbdf0ce0ffd9a3951a4d210
henry-jacq <henryeditz267@gmail.com>
2023-03-05 14:11:39 +0000

    change: error page redesigned
    * Bugs related to styles are fixed
    * changed the colours to match the themes
    * modified as a responsive page

--- htdocs/_templates/_error.php
+++ htdocs/_templates/_error.php
@@ -1,10 +1,12 @@
-<div class="page-wrap d-flex flex-row align-items-center p-5" style="height: 90vh;">
+<div class="d-flex flex-wrap align-items-center justify-content-center" style="height: 100vh;">
     <div class="container p-5">
-        <div class="row mx-auto text-center p-5 bg-secondary rounded">
-                <span class="display-1 d-block text-darkt">404</span>
-                <span class="display-4 d-block mb-3 text-dark">Error</span>
-                <div class="mb-4 lead text-dark">The page you are looking for was not found or other error occured.</div>
-                <a href="/" class="btn btn-link text-black text-decoration-none">← Back to Photogram</a>
+        <div class="row mx-auto text-center py-5 bg-body-tertiary rounded">
+            <span class="display-1 d-block">404</span>
+            <span class="display-4 d-block mb-3">Error</span>
+            <div class="mb-4 lead">The page you are looking for was not found or other error occured.</div>
+            <div class="d-grid col-sm-8 col-md-5 mx-auto">
+                <a href="/" class="btn btn-link text-decoration-none"><i class="bi bi-arrow-left me-2"></i>Back to Photogram</a>
             </div>
+        </div>
     </div>
 </div>
