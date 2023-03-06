c5f5d28ab0390d27e23565007abac551a1bd0148
henry-jacq <henryeditz267@gmail.com>
2023-03-06 16:50:51 +0000

    feat: working breadcrumb model

--- htdocs/_templates/home/breadcrumb.php
+++ htdocs/_templates/home/breadcrumb.php
@@ -2,7 +2,15 @@
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb border rounded p-3">
             <li class="breadcrumb-item"><a href="/" class="text-decoration-none">Home</a></li>
-            <li class="breadcrumb-item active"><a href="#" class="text-decoration-none">User</a></li>
+            <?
+            // echo($_SERVER['REQUEST_URI']);
+            if ($_SERVER['REQUEST_URI'] == '/settings') { ?>
+                <li class="breadcrumb-item active"><a href="/settings" class="text-decoration-none">Settings</a></li>
+                <li class="breadcrumb-item active"><a href="/settings" class="text-decoration-none"><?= ucfirst(Session::getUser()->getUsername()) ?></a></li>
+            <? } elseif ($_SERVER['REQUEST_URI'] == '/profile') { ?>
+                <li class="breadcrumb-item active"><a href="/profile" class="text-decoration-none">Profile</a></li>
+                <li class="breadcrumb-item"><a href="/profile" class="text-decoration-none"><?= ucfirst(Session::getUser()->getUsername()) ?></a></li>
+            <? } ?>
         </ol>
     </nav>
 </div>
