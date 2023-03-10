e8e2b76ec05ae8b872a207ca6738fc43a064f4d9
henry-jacq <henryeditz267@gmail.com>
2023-03-10 16:37:21 +0000

    change: profile page modified

--- htdocs/_templates/home/profile.php
+++ htdocs/_templates/home/profile.php
@@ -1,33 +1,33 @@
-<div class="border-bottom border-secondary shadow-lg">
-    <div class="container p-5">
-        <h3 class="text-light">Your Profile</h3>
-        <hr class="hr text-light p-4">
-        <div class="row">
-            <div class="col-md-4 text-center">
-                <img src="https://api.dicebear.com/5.x/identicon/svg?seed=<?=ucfirst(Session::getUser()->getUsername())?>" class="img-fluid rounded-circle mb-3" alt="Profile Picture" width="200" height="200">
-                <h2 class="text-light"><?=ucfirst(Session::getUser()->getUsername());?></h2>
-                <p class="text-light">Web Developer</p>
-                <button class="btn btn-sm btn-secondary" onclick="dialog('Not Implemented!',' This feature is not implemented');">Change profile icon</button>
+<div class="container p-5">
+    <h3 class="fw-light">Profile Page</h3>
+    <hr class="py-3">
+    <div class="row">
+        <div class="col-md-4 text-center align-middle">
+            <img src="https://api.dicebear.com/5.x/identicon/svg?seed=<?= ucfirst(Session::getUser()->getUsername()) ?>" class="img-fluid border rounded-circle mb-3" alt="Profile Picture" width="175" height="175">
+            <h2 class="fw-normal fs-4"><?= ucfirst(Session::getUser()->getUsername()); ?></h2>
+            <p>Web Developer</p>
+            <button class="btn btn-sm btn-secondary" onclick="dialog('Not Implemented!',' This feature is not implemented');"><i class="bi bi-pencil me-2"></i>Change Profile</button>
+
+            <div class="text-center mt-3">
+                <p class="mt-5 fw-normal fs-5 mb-0">5</p>
+                <p class="fw-light fs-5">My Posts</p>
             </div>
-            <div class="col-md-8">
-                <ul class="list-group border-0 my-3">
-                    <li class="list-group-item bg-dark text-light border-0">Username<br><b>@<?=Session::getUser()->getUsername();?></b></li>
-                    <li class="list-group-item bg-dark text-light border-0">Phone number<br><b><?=Session::getUser()->getPhone();?></b></li>
-                    <li class="list-group-item bg-dark text-light border-0">Email address<br><b><?=Session::getUser()->getEmail()?></b></li>
-                </ul>
-                <div class="card my-3 rounded-3 text-bg-secondary">
-                    <div class="card-body">
-                        <h5 class="card-title text-muted">About Me</h5>
-                        <p class="card-text fw-light">#!/bin/bash<br>Full stack Developer<br>I convert caffeine into code.</p>
-                        <a class="text-decoration-none text-light" href="https://github.com/henry-jacq/">Check Out my Github Page</a>.<br><br>
-                        <i class="text-black">#IoT #electronics #hacking</i>
-                    </div>
-                </div>
-                <i class="text-bg-dark">You can edit your details in <a href="#" class="text-decoration-none">Here</a>.</i>
-            </div>
-            <h3 class="text-light my-3">Your Posts</h3>
-            <hr class="hr text-light p-4">
-            <?=Session::loadTemplate('home/show_posts');?>
         </div>
+        <div class="col-md-8">
+            <ul class="list-group border-0 my-3">
+                <li class="list-group-item border-0">Username<br><b>@<?= Session::getUser()->getUsername(); ?></b></li>
+                <li class="list-group-item border-0">Phone number<br><b><?= Session::getUser()->getPhone(); ?></b></li>
+                <li class="list-group-item border-0">Email address<br><b><?= Session::getUser()->getEmail() ?></b></li>
+            </ul>
+            <div class="container border p-3 rounded">
+                <h5 class="fw-normal">About Me</h5>
+                <p class="fw-light">#!/bin/bash<br>Full stack Developer<br>I convert caffeine into code.</p>
+                <a class="text-decoration-none" href="https://github.com/henry-jacq/">Check Out my Github Page</a>.<br><br>
+                <i>#IoT #electronics #hacking</i>
+            </div>
+        </div>
+        <h3 class="fw-light py-3 mt-3">My Posts</h3>
+        <hr class="p-4">
+        <?= Session::loadTemplate('home/show_posts'); ?>
     </div>
 </div>
