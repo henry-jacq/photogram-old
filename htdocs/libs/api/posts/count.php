fd12bb24607230e8db9a8b697231987146aca7ad
henry-jacq <henryeditz267@gmail.com>
2023-03-12 13:23:52 +0000

    feat: count posts
    * API to count user & all posts
    * counting of posts done if session authenticated
    * mode of counting posts done by GET request

---
+++ htdocs/libs/api/posts/count.php
@@ -0,0 +1,22 @@
+<?php
+
+// Count posts
+// https://{{domain}}/api/posts/count?mode={all|user}
+
+${basename(__FILE__, '.php')} = function () {
+    if ($this->isAuthenticated()) {
+        if ($_GET['mode'] == "all") {
+            $this->response($this->json(Post::countAllPosts()[0]), 200);
+        } elseif ($_GET['mode'] == "user") {
+            $this->response($this->json(Post::countUserPosts(Session::getUser()->getUsername())[0]), 200);
+        } else {
+            $this->response($this->json([
+                'message' => 'Provide proper params to fetch posts'
+            ]), 400);
+        }
+    } else {
+        $this->response($this->json([
+            'message' => "Bad request"
+        ]), 400);
+    }
+};
