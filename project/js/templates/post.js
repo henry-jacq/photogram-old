c9fcf282f874d6634337897be033bbec5c2d0a6e
henry-jacq <henryeditz267@gmail.com>
2023-03-22 16:10:31 +0000

    change: code splitted

---
+++ project/js/templates/post.js
@@ -0,0 +1,36 @@
+// Delete post
+$('.btn-delete').on('click', function(){
+    post_id = $(this).parent().attr('data-id');
+    d = new Dialog("Delete Post ?", "You want to permanently delete this post?");
+    d.setButtons([
+        {
+            'name': "Delete",
+            "class": "btn-danger",
+            "onClick": function(event){             
+                $.post('/api/posts/delete',
+                {
+                    id: post_id
+                }, function(data, textSuccess){
+                    if(textSuccess =="success" ){
+                        $(`#post-${post_id}`).remove();
+                        // window.location.href = "/";
+                        $grid.masonry('layout');
+                        new Toast("Photogram", "1 seconds ago", "Your post was successfully deleted!", {}).show();
+                    } else {
+                        new Toast("Photogram", "1 seconds ago", "cannot delete your post!", {}).show();
+                    }
+                });
+
+                $(event.data.modal).modal('hide')
+            }
+        },
+        {
+            'name': "Cancel",
+            "class": "btn-secondary",
+            "onClick": function(event){
+                $(event.data.modal).modal('hide');
+            }
+        }
+    ]);
+    d.show();
+});
