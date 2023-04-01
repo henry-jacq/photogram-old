01ab565539e3278f59bf54d120f16e92ae9ec09e
henry-jacq <henryeditz267@gmail.com>
2023-04-01 15:21:54 +0000

    code reverted

--- htdocs/_templates/login/index.php
+++ htdocs/_templates/login/index.php
@@ -14,7 +14,6 @@ if (isset($_POST['username_or_email']) and !empty($_POST['username_or_email']) a
 if ($login) {
     // Load the base path, if result has no error
     if ($result) {
-
         $should_redirect = Session::get('_redirect');
         $redirect_to = get_config('base_path');
         if (isset($should_redirect)) {
@@ -23,37 +22,41 @@ if ($login) {
         }
 
         // Save username or email in session
-        Session::set('session_UsernameOrEmail', $_POST['username_or_email']); 
+        Session::set('session_UsernameOrEmail', $_POST['username_or_email']); ?>
 
-        header($redirect_to);
+<script>
+	window.location.href = "<?= $redirect_to ?>"
+</script>
+<?php
+
 
         // If any error, load the same page with popup error
     } else { ?>
 
-        <section class="container">
-            <div class="h-100 d-flex align-items-center justify-content-center row" style="min-height: 100vh;">
-                <div class="py-3 col-sm-10 col-md-8 col-lg-6 col-xl-5 col-xxl-4">
-                    <!-- This will popup the alert -->
-                    <div id="popup-error" class="alert alert-danger alert-dismissible fade show" role="alert">
-                        <strong>Login Failed!</strong><br>Invalid credentials. Please try again.
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                    </div>
-                    <? // Load Login form
+<section class="container">
+	<div class="h-100 d-flex align-items-center justify-content-center row" style="min-height: 100vh;">
+		<div class="py-3 col-sm-10 col-md-8 col-lg-6 col-xl-5 col-xxl-4">
+			<!-- This will popup the alert -->
+			<div id="popup-error" class="alert alert-danger alert-dismissible fade show" role="alert">
+				<strong>Login Failed!</strong><br>Invalid credentials. Please try again.
+				<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+			</div>
+			<?php // Load Login form
                     Session::loadTemplate('login/form_in'); ?>
-                </div>
-            </div>
-        </section>
+		</div>
+	</div>
+</section>
 
-    <? }
+<?php }
     // If the user doesn't submit the form, load the same page
 } else { ?>
-    <section class="container">
-        <div class="h-100 d-flex align-items-center justify-content-center row" style="min-height: 100vh;">
-            <div class="py-3 col-sm-10 col-md-8 col-lg-6 col-xl-5 col-xxl-4">
-                <? // Load Login form
+<section class="container">
+	<div class="h-100 d-flex align-items-center justify-content-center row" style="min-height: 100vh;">
+		<div class="py-3 col-sm-10 col-md-8 col-lg-6 col-xl-5 col-xxl-4">
+			<?php // Load Login form
                 Session::loadTemplate('login/form_in'); ?>
-            </div>
-        </div>
-    </section>
+		</div>
+	</div>
+</section>
 
-<? } ?>
+<?php } ?>
