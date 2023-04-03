3ac4884681dc3991fd89b7b54ec63b4786186262
henry-jacq <henryeditz267@gmail.com>
2023-04-03 09:03:28 +0000

    feat: sanitizing user input values

--- htdocs/_templates/signup/form_up.php
+++ htdocs/_templates/signup/form_up.php
@@ -1,32 +1,34 @@
 <form action="/signup" method="post" autocomplete='off'>
-    <div class="form-control p-4 bg-black bg-opacity-25">
-        <img src="<?= get_config('base_path') ?>assets/brand/photogram-logo.png" alt="logo" class="img-fluid mx-auto d-block mb-2" width="63" height="63">
-        <h4 class="fw-light text-center mb-4">Photogram</h4>
-        <hr class="mb-4">
-        <h5 class="fw-semi-bold mb-4">Register</h5>
+	<div class="form-control p-4 bg-black bg-opacity-25">
+		<img src="<?= get_config('base_path') ?>assets/brand/photogram-logo.png"
+			alt="logo" class="img-fluid mx-auto d-block mb-2" width="63" height="63">
+		<h4 class="fw-light text-center mb-4">Photogram</h4>
+		<hr class="mb-4">
+		<h5 class="fw-semi-bold mb-4">Register</h5>
 
-        <div class="row g-3 mb-3">
-            <div class="col-md-6">
-                <label for="firstName" class="form-label">First name</label>
-                <input type="text" name="first_name" class="form-control bg-transparent" id="firstName" required>
-            </div>
-            <div class="col-md-6">
-                <label for="lastName" class="form-label">Last name</label>
-                <input type="text" name="last_name" class="form-control bg-transparent" id="lastName" required>
-            </div>
-        </div>
+		<div class="row g-3 mb-3">
+			<div class="col-md-6">
+				<label for="firstName" class="form-label">First name</label>
+				<input type="text" name="first_name" class="form-control bg-transparent" id="firstName" required>
+			</div>
+			<div class="col-md-6">
+				<label for="lastName" class="form-label">Last name</label>
+				<input type="text" name="last_name" class="form-control bg-transparent" id="lastName" required>
+			</div>
+		</div>
 
-        <label for="userName" class="form-label">Username</label>
-        <input type="text" id="userName" name="username" class="form-control mb-3 bg-transparent" required>
-        <label for="email" class="form-label">Email</label>
-        <input type="email" id="email" name="email_address" class="form-control mb-3 bg-transparent" required>
-        <label for="password" class="form-label">Password</label>
-        <input type="password" id="password" name="password" class="form-control mb-3 bg-transparent" required>
-        <!-- Fingerprint -->
-        <input name="visitor_id" type="hidden" class="form-control" id="fingerprint" value="">
+		<label for="userName" class="form-label">Username</label>
+		<input type="text" id="userName" name="username" class="form-control mb-3 bg-transparent" required>
+		<label for="email" class="form-label">Email</label>
+		<input type="email" id="email" name="email_address" class="form-control mb-3 bg-transparent" required>
+		<label for="password" class="form-label">Password</label>
+		<input type="password" id="password" name="password" class="form-control mb-3 bg-transparent" required>
+		<!-- Fingerprint -->
+		<input name="visitor_id" type="hidden" class="form-control" id="fingerprint" value="">
 
-        <button type="submit" class="btn btn-primary mb-3 hvr-grow">Register</button>
-        <p class="text-center text-muted mb-0">Do you already have an account? <a href="/login" class="text-decoration-none">Login now</a>.</p>
-        <!-- <p class="text-center text-muted">All rights reserved &copy;2022-2023</p> -->
-    </div>
+		<button type="submit" class="btn btn-primary mb-3 hvr-grow">Register</button>
+		<p class="text-center text-muted mb-0">Do you already have an account? <a href="/login"
+				class="text-decoration-none">Login now</a>.</p>
+		<!-- <p class="text-center text-muted">All rights reserved &copy;2022-2023</p> -->
+	</div>
 </form>
--- htdocs/_templates/signup/index.php
+++ htdocs/_templates/signup/index.php
@@ -1,66 +1,90 @@
 <?php
 
 // Try to register, if the user has submitted the form
-if (isset($_POST['username']) and isset($_POST['password']) and isset($_POST['email_address']) and isset($_POST['first_name']) and isset($_POST['last_name'])) {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $email = $_POST['email_address'];
-    $fname = $_POST['first_name'];
-    $lname = $_POST['last_name'];
-    $result = User::signup($username, $password, $email, $fname, $lname);
-    $signup = true;
-} else {
-    $signup = false;
+$required_fields = array("username" => filter_input(INPUT_POST, 'username', FILTER_SANITIZE_FULL_SPECIAL_CHARS),
+"password" => filter_input(INPUT_POST, 'password', FILTER_SANITIZE_FULL_SPECIAL_CHARS),
+"email_address" => filter_input(INPUT_POST, 'email_address', FILTER_SANITIZE_EMAIL),
+"first_name" => filter_input(INPUT_POST, 'first_name', FILTER_SANITIZE_FULL_SPECIAL_CHARS),
+"last_name" => filter_input(INPUT_POST, 'last_name', FILTER_SANITIZE_FULL_SPECIAL_CHARS));
+
+// Sanitizing user input
+foreach ($required_fields as $key => $value) {
+    if (empty($value)) {
+        $signup = false;
+        break;
+    } else {
+        switch($key) {
+            case 'username':
+                $username = $value;
+                break;
+            case 'password':
+                $password = $value;
+                break;
+            case 'email_address':
+                $email = $value;
+                break;
+            case 'first_name':
+                $fname = $value;
+                break;
+            case 'last_name':
+                $lname = $value;
+                break;
+        }
+        $signup = true;
+    }
 }
 
-// If user submitted the form
+// If user filled and submitted the form
 if ($signup) {
+    $result = User::signup($username, $password, $email, $fname, $lname);
     // Sign up success, if result has no error
     if ($result) { ?>
-        <section class="container">
-            <div class="d-flex align-items-center justify-content-center min-vh-100">
-                <div class="py-3 col-sm-10 col-md-10 col-lg-8 col-xl-6">
-                    <div class="bg-black bg-opacity-25 rounded-3 p-5 text-center border">
-                        <img src="<?= get_config('base_path') ?>assets/brand/photogram-logo.png" alt="logo" class="img-fluid mx-auto d-block mb-4" width="63" height="63">
-                        <h3 class="display-6">Welcome to Photogram!</h3>
-                        <p class="lead mb-4">Your account has been created.</p>
-                        <a class="text-decoration-none" href="/login">
-                            <button class="btn btn-success hvr-icon-forward">Continue to login <i class="fa fa-arrow-right hvr-icon" aria-hidden="true"></i></button>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </section>
+<section class="container">
+	<div class="d-flex align-items-center justify-content-center min-vh-100">
+		<div class="py-3 col-sm-10 col-md-10 col-lg-8 col-xl-6">
+			<div class="bg-black bg-opacity-25 rounded-3 p-5 text-center border">
+				<img src="<?= get_config('base_path') ?>assets/brand/photogram-logo.png"
+					alt="logo" class="img-fluid mx-auto d-block mb-4" width="63" height="63">
+				<h3 class="display-6">Welcome to Photogram!</h3>
+				<p class="lead mb-4">Your account has been created.</p>
+				<a class="text-decoration-none" href="/login">
+					<button class="btn btn-success hvr-icon-forward">Continue to login <i
+							class="fa fa-arrow-right hvr-icon" aria-hidden="true"></i></button>
+				</a>
+			</div>
+		</div>
+	</div>
+</section>
 
-    <?php
-        // If any error, load the same page with popup error
+<?php // If any error, load the same page with popup error
     } else { ?>
 
-        <section class="container">
-            <div class="h-100 d-flex align-items-center justify-content-center row min-vh-100">
-                <div class="py-3 col-sm-10 col-md-8 col-lg-6 col-xl-5 col-xxl-4">
-                    <!-- This will popup the alert -->
-                    <div id="popup-error" class="alert alert-danger alert-dismissible fade show" role="alert">
-                        <strong>Signup Failed!</strong><br>Username or email already exists in database.
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                    </div>
-                    <? // Load Sign up form
-                    Session::loadTemplate('signup/form_up'); ?>
-                </div>
-            </div>
-        </section>
-
-    <?php }
-    // If the user doesn't submit the form, load the same page
-} else { ?>
-
-    <section class="container">
-        <div class="h-100 d-flex align-items-center justify-content-center row min-vh-100">
-            <div class="py-3 col-sm-10 col-md-8 col-lg-6 col-xl-5 col-xxl-4">
-                <? // Load Sign up form
+<section class="container">
+	<div class="h-100 d-flex align-items-center justify-content-center row min-vh-100">
+		<div class="py-3 col-sm-10 col-md-8 col-lg-6 col-xl-5 col-xxl-4">
+			<!-- This will popup the alert -->
+			<div id="popup-error" class="alert alert-danger alert-dismissible fade show" role="alert">
+				<strong>Signup Failed!</strong><br>Username or email already exists in database.
+				<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+			</div>
+			<?php // Load Sign up form
                 Session::loadTemplate('signup/form_up'); ?>
-            </div>
-        </div>
-    </section>
+		</div>
+	</div>
+</section>
+
+<?php }
+    }
+// If the user doesn't submit the form, load the same page
+else { ?>
+
+<section class="container">
+	<div class="h-100 d-flex align-items-center justify-content-center row min-vh-100">
+		<div class="py-3 col-sm-10 col-md-8 col-lg-6 col-xl-5 col-xxl-4">
+			<?php // Load Sign up form
+                Session::loadTemplate('signup/form_up'); ?>
+		</div>
+	</div>
+</section>
 
 <?php } ?>
