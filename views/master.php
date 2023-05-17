5c69b03a2a9cdb5ab8f0e3df6a0589fbf46f2f2a
henry-jacq <henryeditz267@gmail.com>
2023-05-17 17:06:40 +0000

    change: static elements moved to templates
    - The static HTML components moved to seperate template
    - Now the template use included in the master template

--- views/master.php
+++ views/master.php
@@ -11,34 +11,23 @@
 
 	<body class="d-flex flex-column min-vh-100">
 		
-		<noscript><h1>Photogram</h1><p><strong>Photo-sharing web application</strong></p><p data-nosnippet>You need to enable JavaScript to run this app.</p></noscript>
+		<noscript>
+			<h1>Photogram</h1>
+			<p><strong>Photo-sharing web application</strong></p>
+			<p data-nosnippet>You need to enable JavaScript to run this app.</p>
+		</noscript>
 	
 		<?php
 		if (Session::$isError) {
+			// If it is error, load the error page
 			View::loadErrorPage();
 		} else {
+			// If no error, load the current script
 			View::loadTemplate(Session::currentScript());
 		}
-		?>
-
-		<div id="modalsGarbage">
-			<div class="modal fade user-select-none" data-bs-backdrop="static" id="dummy-dialog-modal" tabindex="-1" role="dialog"
-				aria-labelledby="" aria-hidden="true">
-				<div class="modal-dialog modal-dialog-centered">
-					<div class="modal-content blur">
-						<div class="modal-header">
-							<h4 class="modal-title"></h4>
-						</div>
-						<div class="modal-body">
-						</div>
-						<div class="modal-footer">
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<?php
+		// Load the static elements
+		View::loadTemplate('templates/elements');
+		// Link javascript files
 		View::loadTemplate('templates/load_js');
 		?>
 
