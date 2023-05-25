994146d2b8a1c7754ad4b7533989197266964c58
henry-jacq <henryeditz267@gmail.com>
2023-05-25 14:15:02 +0000

    test: Checking PDO driver

--- public/test.php
+++ public/test.php
@@ -4,9 +4,29 @@ use App\Core\User;
 
 include 'libs/autoload.php';
 
+$servername = DB_HOST;
+$username = DB_USER;
+$password = DB_PASS;
+$database = DB_NAME;
 
 try {
-    User::changePassword('test@mail.com', 'testing');
-} catch (Exception $e) {
-    echo $e->getMessage();
+    $db = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $query = "SELECT * FROM `auth` WHERE `email` = ?";
+
+    $stmt = $db->prepare($query);
+
+    $stmt->bindValue(1, '');
+
+    $stmt->execute();
+
+    echo '<pre>';
+    print_r($stmt->fetchAll()[0]);
+    echo '</pre>';
+    // exit;
+    
+    
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage());
 }
