ef777c208b0002c3b58ce2565223eb9f6cb1e842
henry-jacq <henryeditz267@gmail.com>
2023-05-28 05:41:21 +0000

    change: New columns added to users table
    - New columns first_name and last_name added
    - User ID set as unique key
    - Some columns have default value as null

--- database/migrations/m007_users.php
+++ database/migrations/m007_users.php
@@ -7,14 +7,19 @@ class m007_users {
     {
         $db = new Database();
         $sql = "CREATE TABLE `users` (
+            `id` int NOT NULL AUTO_INCREMENT,
             `uid` int NOT NULL,
-            `bio` longtext NOT NULL,
-            `avatar` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+            `first_name` tinytext NOT NULL,
+            `last_name` tinytext NOT NULL,
+            `bio` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+            `job` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+            `avatar` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
             `sec_email` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-            `instagram` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+            `location` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
             `twitter` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-            `facebook` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-            KEY `id` (`uid`),
+            `instagram` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+            PRIMARY KEY (`id`),
+            UNIQUE KEY `id` (`uid`),
             CONSTRAINT `users_ibfk_3` FOREIGN KEY (`uid`) REFERENCES `auth` (`id`) ON DELETE CASCADE ON UPDATE SET DEFAULT
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;";
         $db->prepare($sql);
