607b698c458aa1ec6aaf331dd24e2e4eaf5747f1
henry-jacq <henryeditz267@gmail.com>
2023-06-07 18:18:19 +0000

    feat: New API endpoint for reset password
    - New endpoint for changing user password
    - This API will check the email and returns exists or not
    - Also it will change the password if all the required fields are filled

---
+++ public/libs/api/auth/reset-password.php
@@ -0,0 +1,61 @@
+<?php
+
+// Reset user password
+// https://{{domain}}/api/auth/reset-password
+
+use App\Core\Mailer;
+use App\Core\Session;
+use App\Core\User;
+
+${basename(__FILE__, '.php')} = function () {
+    if (!$this->isAuthenticated() && $this->paramsExists(['reset_email'])) {
+        $mail_to = $this->_request['reset_email'];
+        if (Mailer::mailExists($mail_to)) {
+            $link = Mailer::sendPasswordResetMail($mail_to);
+            if (isset($link)) {
+                $this->response($this->json([
+                    'message'=>'Mail sent!',
+                    'link'=>$link,
+                    'status'=>'Success'
+                ]), 200);
+            } else {
+                $this->response($this->json([
+                    'message'=>'Mailer error!',
+                    'link'=>$link,
+                    'status'=>'Failed'
+                ]), 401);
+            }
+        } else {
+            $this->response($this->json([
+                'message'=>'Mail cannot be sent!',
+                'status'=>'Failed'
+            ]), 401);
+        }
+    } else if (!$this->isAuthenticated() && $this->paramsExists(['newPassword', 'confirmNewPassword'])) {
+        $email = Session::get('reset_password_email');
+        $new = $this->_request['newPassword'];
+        $confirmNew = $this->_request['confirmNewPassword'];
+
+        if (!empty($new) && !empty($confirmNew && $new === $confirmNew)) {
+            if (User::changePassword($email, $new) && User::revokeResetToken($email)) {
+                $this->response($this->json([
+                    'message'=>'Password changed!',
+                    'status'=>'Success'
+                ]), 200);
+            } else {
+                $this->response($this->json([
+                    'message'=>'Cannot change the password!',
+                    'status'=>'Failed'
+                ]), 200);
+            }
+        } else {
+            $this->response($this->json([
+                'message'=>"Password not matches"
+            ]), 401);
+        }
+    } else {
+        $this->response($this->json([
+            'message'=>"bad request"
+        ]), 400);
+    }
+};
