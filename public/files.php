e4b4c681af51ba53573cb78548334f4ed81cf87d
henry-jacq <henryeditz267@gmail.com>
2023-06-25 03:25:13 +0000

    chore: Improved file retrieval & handle namespaces
    - Added checks for valid namespaces to ensure handling of file requests
    - This changes will improve the code's maintainability and security

--- public/files.php
+++ public/files.php
@@ -2,35 +2,38 @@
 
 include '../bootstrap.php';
 
-use App\Core\Session;
 use App\Core\View;
 
-if (isset($_GET['file'])) {
-    $fname = $_GET['file'];
-    
-    // Determine the upload path based on the namespace
-    if ($_GET['namespace'] == 'posts') {
-        $path = APP_STORAGE_PATH . '/posts/';
-    } elseif ($_GET['namespace'] == 'avatars') {
-        $path = APP_STORAGE_PATH . '/avatars/';
-    } else {
-        die();
-    }
-    
-    // To prevent directory traversal
-    $fname = str_replace('..', '', $fname);
-    
-    $image_path = $path . $fname;
+if (!isset($_GET['file'])) {
+    View::renderPage();
+    exit;
+}
 
-    if (is_file($image_path)) {
-        header("Content-Type:" . mime_content_type($image_path));
-        header("Content-Length:" . filesize($image_path));
-        header('Cache-control: max-age=' . (60 * 60 * 24 * 365));
-        header('Expires: ' . gmdate(DATE_RFC1123, time() + 60 * 60 * 24 * 365));
-        header('Last-Modified: ' . gmdate(DATE_RFC1123, filemtime($image_path)));
-        header_remove('Pragma');
-        echo(file_get_contents($image_path));
-    }
+$namespace = $_GET['namespace'] ?? '';
+$fname = $_GET['file'] ?? '';
+
+$validNamespaces = ['posts', 'avatars'];
+
+if (!in_array($namespace, $validNamespaces)) {
+    View::renderPage();
+    exit;
+}
+
+$path = APP_STORAGE_PATH . `/{$namespace}/`;
+
+// To prevent directory traversal
+$fname = str_replace('..', '', $fname);
+
+$imagePath = "$path/$namespace/$fname";
+
+if (file_exists($imagePath) && is_file($imagePath)) {
+    header("Content-Type:" . mime_content_type($imagePath));
+    header("Content-Length:" . filesize($imagePath));
+    header('Cache-control: max-age=' . (60 * 60 * 24 * 365));
+    header('Expires: ' . gmdate(DATE_RFC1123, time() + 60 * 60 * 24 * 365));
+    header('Last-Modified: ' . gmdate(DATE_RFC1123, filemtime($imagePath)));
+    header_remove('Pragma');
+    echo file_get_contents($imagePath);
 } else {
     View::renderPage();
 }
