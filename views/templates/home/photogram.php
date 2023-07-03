0812ec6d27524a78e3f9835977e21355caf1d989
henry-jacq <henryeditz267@gmail.com>
2023-07-03 09:52:11 +0000

    change: stories section removed

--- views/templates/home/photogram.php
+++ views/templates/home/photogram.php
@@ -7,11 +7,6 @@ use App\Core\View;
 
 <div class="album py-3">
 	<div class="container">
-		<?php
-		if (Session::isAuthenticated() && Session::currentScript() == "index") {
-			View::renderTemplate('templates/home/stories');
-		}		
-		?>
 		<?=View::renderTemplate('templates/home/show_posts');?>
 	</div>
 </div>
